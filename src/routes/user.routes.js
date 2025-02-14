897455bffbe0a63c96a11a562a78dc8b0ffe9357
Avani Agarwal <102690647+Avani011@users.noreply.github.com>
2025-02-15 01:40:02 +0530

    Acces Token and Refresh Token Creation and Updation

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {refreshAccessToken} from "../controllers/user.controller.js"
 
 const router = express.Router();
 
@@ -18,6 +20,14 @@ router.route("/register").post(
     registerUser
 );
 
+router.route("/login").post(loginUser);
+
 // router.post("/register", registerUser);
+
+//secured routes
+
+router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router
